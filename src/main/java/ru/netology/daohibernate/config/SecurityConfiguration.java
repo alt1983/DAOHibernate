856e2e80952c1386b6e4942cfd16be3548d0fd4a
alt1983 <timofeev_a_v@mail.ru>
2022-12-18 16:16:59 +0300

    Commit 10

--- src/main/java/ru/netology/daohibernate/config/SecurityConfiguration.java
+++ src/main/java/ru/netology/daohibernate/config/SecurityConfiguration.java
@@ -1,2 +1,47 @@
-package ru.netology.daohibernate.config;public class SecurityConfiguration {
+package ru.netology.daohibernate.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/persons/by-city").permitAll()
+                        .requestMatchers("/persons/by-age").hasRole("USER")
+                        .requestMatchers("/persons/by-name-and-surname").hasRole("ADMIN")
+                        .anyRequest().denyAll()
+                ).formLogin(withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withDefaultPasswordEncoder()
+                .username("user1")
+                .password("user1")
+                .roles("USER")
+                .build();
+        UserDetails user2 = User.withDefaultPasswordEncoder()
+                .username("user2")
+                .password("user2")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
 }
